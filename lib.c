5ac02094332470e27c4b996f7c218bf47a150a48
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-06-03 19:40:59 +0900

    tuned performance with caching

--- lib.c
+++ lib.c
@@ -1,7 +1,6 @@
 #include"ext.h"            // standard Max include, always required (except in Jitter)
 #include"ext_obex.h"        // required for "new" style objects
 #include"z_dsp.h"            // required for MSP objects
-#include"commonsyms.h"
 #include<Accelerate/Accelerate.h>
 #include<simd/simd.h>
 typedef struct {
@@ -11,23 +10,26 @@ typedef struct {
 	uintptr_t const length;
 	double const damping;
 	simd_double2 const boundary;
-	simd_double3 const strength;
+	simd_double4 const strength;
 	double * const memory;
 } t_waveeq1d;
 C74_HIDDEN static t_class const * class = NULL;
+C74_HIDDEN inline double __attribute__((always_inline)) fix(double const x) {
+	return isnormal(x) ? x : 0;
+}
 C74_HIDDEN void*new(t_symbol const * const symbol, long const argc, t_atom const * const argv) {
 	t_waveeq1d * const this = (t_waveeq1d * const)object_alloc((t_class*const)class);
     if ( this ) {
 		// attr
 		*(double*const)(&this->damping) = 0;
 		*(simd_double2*const)(&this->boundary) = simd_make_double2(0, 0);
-		*(simd_double3*const)(&this->strength) = simd_make_double3(0.5, 0, -0.5);
+		*(simd_double4*const)(&this->strength) = simd_make_double4(0.5, 0, -0.5, 1);
 		attr_args_process(this, argc, (t_atom*const)argv);
 		*(double const**const)(&this->memory) = (double*const)sysmem_newptr(0);
 		
 		// init
         z_dsp_setup((t_pxobject*const)&this->super, 2);
-		((t_pxobject*const)&this->super)->z_misc |= Z_MC_INLETS;//|Z_NO_INPLACE;
+		((t_pxobject*const)&this->super)->z_misc |= Z_MC_INLETS|Z_NO_INPLACE;
         outlet_new(this, "multichannelsignal");
     }
     return this;
@@ -48,6 +50,79 @@ C74_HIDDEN bool inputchanged(t_waveeq1d const * const this, long const index, lo
 }
 C74_HIDDEN long multichanneloutputs(t_waveeq1d const * const this) {
 	return this->length;
+}
+C74_HIDDEN void routine64(t_waveeq1d * const this, t_object const * const dsp64, double const * const * const ins, long const numins, double * const * const outs, long const numouts, long const framecount, long const flags, void const * const userparam) {
+	register uintptr_t const N = this->length;
+	register uintptr_t const A = this->inputs[0];
+	register uintptr_t const Y = this->inputs[1];
+	C74_ASSERT(numouts == N);
+	C74_ASSERT(numins == A + Y);
+	register double const * const * const a = ins;
+	register double const * const * const y = ins + A;
+	register double * const up = this->memory + 0 * N;
+	register double * const uc = this->memory + 1 * N;
+	register double * const uf = this->memory + 2 * N;
+	register double * const ap = this->memory + 3 * N;
+	register double * const ac = this->memory + 4 * N;
+	register double * const af = this->memory + 5 * N;
+	register simd_double4 const w = this->strength;
+	double const r = pow(10, 0.05 * this->damping / (uintptr_t const)userparam);
+#pragma omp parallel for
+	for ( register uintptr_t k = 0, K = N ; k < K ; ++ k )
+		vDSP_vdivD(y[k%Y], 1, a[k%A], 1, outs[k], 1, framecount);
+	for ( register uintptr_t t = 0, T = framecount ; t < T ; ++ t ) {
+		// boundaries
+		outs[  0][t] = uf[  0] = simd_dot(w, (simd_double4 const){
+			af[  0] = fix(outs[  0][t]),
+			ac[  0],
+			ap[  0],
+			simd_dot((simd_double4 const) {
+				1 + fix(( y[(  1)%Y][t] - y[(  0)%Y][t] ) / ( y[(  1)%Y][t] + y[(  0)%Y][t] )),
+				1,
+			}, (simd_double4 const) {
+				uc[  1] - uc[  0],
+				uc[  0],
+			})
+		});
+		outs[N-1][t] = uf[N-1] = simd_dot(w, (simd_double4 const){
+			af[N-1] = fix(outs[N-1][t]),
+			ac[N-1],
+			ap[N-1],
+			simd_dot((simd_double4 const) {
+				1 + fix(( y[(N-2)%Y][t] - y[(N-1)%Y][t] ) / ( y[(N-2)%Y][t] + y[(N-1)%Y][t] )),
+				1,
+			}, (simd_double4 const) {
+				uc[N-2] - uc[N-1],
+				uc[N-1]
+			})
+		});
+		// propagation
+		for ( register uintptr_t k = 1, K = N - 1 ; k < K ; ++ k )
+			outs[k][t] = uf[k] = simd_dot(w, (simd_double4 const) {
+				af[k] = fix(outs[k][t]),
+				ac[k],
+				ap[k],
+				simd_dot((simd_double4 const) {
+					1 + fix(( y[(k-1)%Y][t] - y[k%Y][t] ) / ( y[(k-1)%Y][t] + y[k%Y][t] )),
+					1 + fix(( y[(k+1)%Y][t] - y[k%Y][t] ) / ( y[(k+1)%Y][t] + y[k%Y][t] )),
+					2,
+					-1,
+				}, (simd_double4 const) {
+					uc[k-1] - uc[k],
+					uc[k+1] - uc[k],
+					uc[k],
+					up[k]
+				})
+			});
+		// update
+		vDSP_vsmulD(uc, 1, &r, up, 1, N);
+		vDSP_vsmulD(uf, 1, &r, uc, 1, N);
+		vDSP_vsmulD(ac, 1, &r, ap, 1, N);
+		vDSP_vsmulD(af, 1, &r, ac, 1, N);
+	}
+#pragma omp parallel for
+	for ( register uintptr_t k = 0, K = N ; k < K ; ++ k )
+		vDSP_vmulD(y[k%Y], 1, outs[k], 1, outs[k], 1, framecount);
 }
 C74_HIDDEN void perfroutine64(t_waveeq1d * const this, t_object const * const dsp64, double const * const * const ins, long const numins, double * const * const outs, long const numouts, long const framecount, long const flags, void const * const userparam) {
 	register uintptr_t const N = this->length;
@@ -90,8 +165,11 @@ C74_HIDDEN void perfroutine64(t_waveeq1d * const this, t_object const * const ds
 		// w    <- in2
 		for ( register uintptr_t n = 0 ; n < N ; ++ n ) {
 			w[n] = z[n % Z][t];
-			a[k][n] = isnormal(w[n]) ? x[n % X][t] / w[n] : 0;
+			a[k][n] = x[n % X][t];
 		}
+		
+		vDSP_vdivD(w, 1, a[k], 1, a[k], 1, N);
+		vDSP_viclipD(a[k], 1, (double const[]){-0}, (double const[]){ 0}, a[k], 1, N);
 		
 		// m[0] <- weight_{k+1} + weight_{k}
 		// m[1] <- weight_{k+1} - weight_{k}
@@ -107,8 +185,7 @@ C74_HIDDEN void perfroutine64(t_waveeq1d * const this, t_object const * const ds
 				   m[2], 1, 
 				   N - 1);
 		
-		for ( register uintptr_t n = 0 ; n < N ; ++ n )
-			m[2][n] = isnormal(m[2][n]) ? m[2][n] : 0;
+		vDSP_viclipD(m[2], 1, (double const[]){-0}, (double const[]){ 0}, m[2], 1, N);
 		
 		// m[0] <- 1 + m[2]
 		// m[1] <- 1 - m[2]
@@ -162,17 +239,20 @@ C74_HIDDEN void perfroutine64(t_waveeq1d * const this, t_object const * const ds
 		vDSP_vsmulD(a[j], 1, &r, a[j], 1, N);
 		vDSP_vsmulD(a[k], 1, &r, a[k], 1, N);
 		
-		for ( register uintptr_t n = 0 ; n < N ; ++ n ) {
+		for ( register uintptr_t n = 0 ; n < N ; ++ n )
 			outs[n][t] = w[n] * u[k][n];
-		}
 		
 	}
 	*(uintptr_t*const)&this->elapse = tau % 3;
 }
 C74_HIDDEN void dsp64(t_waveeq1d const * const this, t_object const * const dsp64, short const * const count, double const samplerate, long const maxvectorsize, long const flags) {
-	*(double const**const)&this->memory = (double*const)sysmem_resizeptrclear(this->memory, 10 * this->length * sizeof(double const));
+	
 	*(uintptr_t*const)&this->elapse = 0;
-	dsp_add64((t_object*const)dsp64, (t_object*const)this, (t_perfroutine64 const)perfroutine64, 0, (uintptr_t const)samplerate);
+//	*(double const**const)&this->memory = (double*const)sysmem_resizeptrclear(this->memory, 10 * this->length * sizeof(double const));
+//	dsp_add64((t_object*const)dsp64, (t_object*const)this, (t_perfroutine64 const)perfroutine64, 0, (uintptr_t const)samplerate);
+	
+	*(double const**const)&this->memory = (double*const)sysmem_resizeptrclear(this->memory, 6 * this->length * sizeof(double const));
+	dsp_add64((t_object*const)dsp64, (t_object*const)this, (t_perfroutine64 const)routine64, 0, (uintptr_t const)samplerate);
 }
 C74_EXPORT void ext_main(void * const _) {
     if ( !class ) {
